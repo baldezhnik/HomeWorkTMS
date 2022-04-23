3dab71108d24bcea3b366e751b72829b8b76257e
baldezhnik <drako.dima@gmail.com>
2022-04-23 12:02:27 +0300

    Добавленно задание 14-3

--- app/src/main/java/com/dsd/homework/HW14.kt
+++ app/src/main/java/com/dsd/homework/HW14.kt
@@ -8,7 +8,6 @@ class HW14 {
         }
     }
     private var usersList: MutableList<User> = mutableListOf()
-
     private fun addUser (Name: String, secondName: String, age:Int) =
         usersList.add(User(Name,secondName,age))
     private fun outUsers(): String =
@@ -32,4 +31,27 @@ class HW14 {
             else -> "Вопрос задан неверно"
         }
     }
+
+    inner class Zagadka (private val zagadka: String,
+                         private val otvet: String,
+                         private val nePrav1: String,
+                         private val nePrav2: String){
+        fun getZagadka () : String {return zagadka}
+        fun getOtvet() : String {return otvet}
+        fun getNePrav1() :String {return nePrav1}
+        fun getNePrav2() : String {return nePrav2}
+    }
+    val zagadkaList: List<Zagadka> = listOf(Zagadka("Не лёд, а тает, не лодка, а уплывает.",
+                                                     "Зарплата",
+                                                     "Вода",
+                                                    "Нос"),
+        Zagadka("Что нужно делать, когда видишь зеленого человечка?",
+            "Переходить дорогу",
+            "Молиться",
+            "Бежать"),
+        Zagadka("Какой болезнью никто не болеет на суше?",
+            "Морской",
+            "Ветрянкой",
+            "Оспой"),)
+
 }
--- app/src/main/java/com/dsd/homework/MainActivity.kt
+++ app/src/main/java/com/dsd/homework/MainActivity.kt
@@ -14,9 +14,59 @@ class MainActivity : AppCompatActivity() {
         val textView: TextView = findViewById(R.id.textView)
         textView.text = hw14.doHomeWork1()
 
+
         val button: Button = findViewById(R.id.button)
         val textView5: TextView = findViewById(R.id.textView5)
         button.setOnClickListener { textView5.text = hw14.doHomeWork2() }
+
+        val button2: Button = findViewById(R.id.button2)
+        val button3: Button = findViewById(R.id.button3)
+        val button4: Button = findViewById(R.id.button4)
+        var numZag = zapolnitZagadku()
+        button2.setOnClickListener { checkOtvet(numZag, button2.text as String)
+            numZag = zapolnitZagadku()}
+        button3.setOnClickListener { checkOtvet(numZag, button3.text as String)
+            numZag = zapolnitZagadku()}
+        button4.setOnClickListener { checkOtvet(numZag, button4.text as String)
+            numZag = zapolnitZagadku()}
+
+    }
+    private fun zapolnitZagadku () : Int{
+        val hw14 = HW14()
+        val textView7: TextView = findViewById(R.id.textView7)
+        val button2: Button = findViewById(R.id.button2)
+        val button3: Button = findViewById(R.id.button3)
+        val button4: Button = findViewById(R.id.button4)
+        val i :Int = (0..2).random()
+        textView7.text = hw14.zagadkaList[i].getZagadka()
+        when ((1..6).random()){
+            1-> {button2.text = hw14.zagadkaList[i].getOtvet()
+                button3.text = hw14.zagadkaList[i].getNePrav1()
+                button4.text = hw14.zagadkaList[i].getNePrav2()}
+            2->{button2.text = hw14.zagadkaList[i].getOtvet()
+                button4.text = hw14.zagadkaList[i].getNePrav1()
+                button3.text = hw14.zagadkaList[i].getNePrav2()}
+            3->{button3.text = hw14.zagadkaList[i].getOtvet()
+                button2.text = hw14.zagadkaList[i].getNePrav1()
+                button4.text = hw14.zagadkaList[i].getNePrav2()}
+            4->{button3.text = hw14.zagadkaList[i].getOtvet()
+                button4.text = hw14.zagadkaList[i].getNePrav1()
+                button2.text = hw14.zagadkaList[i].getNePrav2()}
+            5->{button4.text = hw14.zagadkaList[i].getOtvet()
+                button2.text = hw14.zagadkaList[i].getNePrav1()
+                button3.text = hw14.zagadkaList[i].getNePrav2()}
+            else -> {button4.text = hw14.zagadkaList[i].getOtvet()
+                button3.text = hw14.zagadkaList[i].getNePrav1()
+                button2.text = hw14.zagadkaList[i].getNePrav2()}
+        }
+        return i
+    }
+    private fun checkOtvet (numZag:Int, chStr:String){
+        val textView8: TextView = findViewById(R.id.textView8)
+        val hw14 = HW14()
+        if (chStr == hw14.zagadkaList[numZag].getOtvet()) textView8.text = "Правильно"
+            else textView8.text = "Неправильно"
+
 
     }
 }
