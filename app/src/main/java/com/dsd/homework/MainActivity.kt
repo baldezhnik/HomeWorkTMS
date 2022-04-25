49cdedfb69c879d60eeb147969051705d59afab1
baldezhnik <drako.dima@gmail.com>
2022-04-25 22:05:15 +0300

    Создана ветка для выполнения HW16

--- app/src/main/java/com/dsd/homework/MainActivity.kt
+++ app/src/main/java/com/dsd/homework/MainActivity.kt
@@ -4,16 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        activityHW14()
     }
 
-    fun activityHW14(view: View) {
-        val intent = Intent(this, HW14Activity::class.java)
-        startActivity(intent)
+    private fun activityHW14() {
+        val button: Button = findViewById(R.id.btn_hw14)
+        button.setOnClickListener {
+            val intent = Intent(this, HW14Activity::class.java)
+            startActivity(intent)
+        }
     }
 
     fun activityHW15(view: View) {
