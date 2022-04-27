171eeb216588a476a6dd844e3d3d6ab645016108
baldezhnik <drako.dima@gmail.com>
2022-04-27 19:59:52 +0300

    Изменения согласно замечаниям изменены переменные и индексы TextView изменена функция в 4 задании с if на when

--- app/src/main/java/com/dsd/homework/HW16Activity.kt
+++ app/src/main/java/com/dsd/homework/HW16Activity.kt
@@ -13,11 +13,11 @@ class HW16Activity : AppCompatActivity() {
 
         //#1
         val btnAct16HW1: Button = findViewById(R.id.btn_act16_hw1)
-        val txtvAct16HW1: TextView = findViewById(R.id.txtv_act16_hw1)
+        val tvAct16HW1: TextView = findViewById(R.id.tv_act16_hw1)
         btnAct16HW1.setOnClickListener {
             val a = (0..100).random()
             val b = (0..100).random()
-            txtvAct16HW1.text = when (a % 2) {
+            tvAct16HW1.text = when (a % 2) {
                 0 -> {
                     "a = $a четное, b = $b, a*b = ${a * b}"
                 }
@@ -29,7 +29,7 @@ class HW16Activity : AppCompatActivity() {
 
         //#2
         val btnAct16HW2: Button = findViewById(R.id.btn_act16_hw2)
-        val txtvAct16HW2: TextView = findViewById(R.id.txtv_act16_hw2)
+        val tvAct16HW2: TextView = findViewById(R.id.tv_act16_hw2)
         btnAct16HW2.setOnClickListener {
             val a = (0..10).random()
             val b = (0..10).random()
@@ -37,12 +37,12 @@ class HW16Activity : AppCompatActivity() {
             val s: (Int, Int, Int) -> Int = { a, b, c ->
                 max((a * b * c), (a + b + c))
             }
-            txtvAct16HW2.text = "a=$a, b=$b, c=$c, max=${s(a, b, c)}"
+            tvAct16HW2.text = "a=$a, b=$b, c=$c, max=${s(a, b, c)}"
         }
 
         //#3
         val btnAct16HW3: Button = findViewById(R.id.btn_act16_hw3)
-        val txtvAct16HW3: TextView = findViewById(R.id.txtv_act16_hw3)
+        val tvAct16HW3: TextView = findViewById(R.id.tv_act16_hw3)
         btnAct16HW3.setOnClickListener {
             val studentRating = ((0..100).random())
             val studentMark = when (studentRating) {
@@ -54,29 +54,28 @@ class HW16Activity : AppCompatActivity() {
                 in (90..100) -> 'A'
                 else -> "Вне диапазона"
             }
-            txtvAct16HW3.text = "У студента с рейтингом $studentRating отметка $studentMark"
+            tvAct16HW3.text = "У студента с рейтингом $studentRating отметка $studentMark"
         }
 
         //#4
         val btnAct16HW4: Button = findViewById(R.id.btn_act16_hw4)
-        val txtvAct16HW4: TextView = findViewById(R.id.txtv_act16_hw4)
+        val tvAct16HW4: TextView = findViewById(R.id.tv_act16_hw4)
         btnAct16HW4.setOnClickListener {
-            val a1 = (0..20).random()
-            val b1 = (0..20).random()
-            val a2 = (0..20).random()
-            val b2 = (0..20).random()
-            val str: String
-            if (((a1 > a2) and (b1 > b2)) or ((a1 > b2) and (b1 > a2))) str =
-                "В 1-ый можно поместить 2-ой."
-            else if (((a1 < a2) and (b1 < b2)) or ((a1 < b2) and (b1 < a2))) str =
-                "В 1-ый можно поместить 2-ой."
-            else str = "Нельзя вместить конверты"
-            txtvAct16HW4.text = "1-ый конверт со сторнами ($a1;$b1). 2-ой ($a2;$b2). $str"
+            val a1 = (1..20).random()
+            val b1 = (1..20).random()
+            val a2 = (1..20).random()
+            val b2 = (1..20).random()
+            val str = when {
+                ((a1 > a2) and (b1 > b2)) or ((a1 > b2) and (b1 > a2)) -> "В 1-ый можно поместить 2-ой."
+                ((a1 < a2) and (b1 < b2)) or ((a1 < b2) and (b1 < a2)) -> "Во 2-ой можно поместить 1-ый."
+                else -> "Нельзя вместить конверты"
+            }
+            tvAct16HW4.text = "1-ый конверт со сторнами ($a1;$b1). 2-ой ($a2;$b2). $str"
         }
 
         //#5
         val btnAct16HW5: Button = findViewById(R.id.btn_act16_hw5)
-        val txtvAct16HW5: TextView = findViewById(R.id.txtv_act16_hw5)
+        val tvAct16HW5: TextView = findViewById(R.id.tv_act16_hw5)
         btnAct16HW5.setOnClickListener {
             var amountEven = 0
             var sumEven = 0
@@ -86,24 +85,24 @@ class HW16Activity : AppCompatActivity() {
                     amountEven++
                 }
             }
-            txtvAct16HW5.text =
+            tvAct16HW5.text =
                 "В диапазоне [1; 99] количество четных чисел $amountEven с общей суммой $sumEven"
         }
 
         //#6
         val btnAct16HW6: Button = findViewById(R.id.btn_act16_hw6)
-        val txtvAct16HW6: TextView = findViewById(R.id.txtv_act16_hw6)
+        val tvAct16HW6: TextView = findViewById(R.id.tv_act16_hw6)
         btnAct16HW6.setOnClickListener {
             val fac = (1..20).random()
-            txtvAct16HW6.text = "факториал $fac! = ${factorial(fac)}"
+            tvAct16HW6.text = "факториал $fac! = ${factorial(fac)}"
         }
 
         //#7
         val btnAct16HW7: Button = findViewById(R.id.btn_act16_hw7)
-        val txtvAct16HW7: TextView = findViewById(R.id.txtv_act16_hw7)
+        val tvAct16HW7: TextView = findViewById(R.id.tv_act16_hw7)
         btnAct16HW7.setOnClickListener {
             val num = (1..100).random()
-            txtvAct16HW7.text = "число $num в двоичной системе ${Integer.toBinaryString(num)}"
+            tvAct16HW7.text = "число $num в двоичной системе ${Integer.toBinaryString(num)}"
         }
     }
 
