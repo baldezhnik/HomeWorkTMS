71fbf851bce695aded232e1ec8be405de1ec1fe6
baldezhnik <drako.dima@gmail.com>
2022-05-02 20:15:11 +0300

    Изменения согласно замечаниям 1. Инициализация переменных выполнено в отдельном методе 2. Ничего не ввести впринципе невозможно, из-за использования seekBar и начальных значений. Так же в seekBar есть проверка на null.

--- app/src/main/java/com/dsd/homework/HW17Activity.kt
+++ app/src/main/java/com/dsd/homework/HW17Activity.kt
@@ -11,21 +11,27 @@ import androidx.core.view.isVisible
 
 class HW17Activity : AppCompatActivity() {
 
-    private var timer: CountDownTimer?= null
+    private var timer: CountDownTimer? = null
+    lateinit var tvAct17HW1: TextView
+    lateinit var tvAct17HW2: TextView
+    lateinit var btnAct17HW1: Button
+    lateinit var pBar: ProgressBar
+    lateinit var seekBar: SeekBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hw17)
-        val tvAct17HW1: TextView = findViewById(R.id.tv_act17_hw1)
-        val pBar :ProgressBar = findViewById(R.id.progressBar2)
-        var seek: Int = 0
-        val seekBar: SeekBar = findViewById(R.id.seekBar2)
+
+        initView()
+        var seek: Int = 3
         tvAct17HW1.text = seekBar.progress.toString()
-        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, p1: Int, p2: Boolean) {
             }
+
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
             }
+
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
                 if (seekBar != null) {
                     seek = seekBar.progress
@@ -34,24 +40,29 @@ class HW17Activity : AppCompatActivity() {
             }
         })
 
-        val btnAct17HW1 : Button = findViewById(R.id.btn_act17_hw1)
+        val btnAct17HW1: Button = findViewById(R.id.btn_act17_hw1)
         btnAct17HW1.setOnClickListener {
             pBar.visibility = ProgressBar.VISIBLE
-            pBar.max = seek*1000
-            startCountDawnTimer((seek*1000).toLong())
+            pBar.max = seek * 1000
+            startCountDawnTimer((seek * 1000).toLong())
         }
     }
 
-    private fun startCountDawnTimer(timeMillis: Long){
-        val tvAct17HW2: TextView = findViewById(R.id.tv_act17_hw2)
-        val pBar :ProgressBar = findViewById(R.id.progressBar2)
+    private fun initView() {
+        tvAct17HW1 = findViewById(R.id.tv_act17_hw1)
+        tvAct17HW2 = findViewById(R.id.tv_act17_hw2)
+        btnAct17HW1 = findViewById(R.id.btn_act17_hw1)
+        pBar = findViewById(R.id.progressBar2)
+        seekBar = findViewById(R.id.seekBar2)
+    }
 
+    private fun startCountDawnTimer(timeMillis: Long) {
         timer?.cancel()
         timer = object : CountDownTimer(timeMillis, 1) {
-            override fun onTick(p0: Long) {
-                val timeSec :Double = (p0).toDouble()/1000
+            override fun onTick(progress: Long) {
+                val timeSec: Double = (progress).toDouble() / 1000
                 tvAct17HW2.text = "Осталось ${String.format("%.2f", timeSec)} секунд"
-                pBar.progress = (timeMillis - p0).toInt()
+                pBar.progress = (timeMillis - progress).toInt()
             }
 
             override fun onFinish() {
