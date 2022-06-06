9f631a4e193f511b16bf46005d148d0485f7a0e7
baldezhnik <drako.dima@gmail.com>
2022-06-06 16:07:03 +0300

    Удалены Activity

--- app/src/main/java/com/dsd/homework/MainActivity.kt
+++ app/src/main/java/com/dsd/homework/MainActivity.kt
@@ -1,14 +1,7 @@
 package com.dsd.homework
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
-import com.dsd.homework.hw18.HW18Activity
-import com.dsd.homework.hw19.HW19Activity
 
 class MainActivity : AppCompatActivity() {
 
